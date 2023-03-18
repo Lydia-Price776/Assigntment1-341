6fce91dfbcf48789e5f98ec99ae3c894c8a0b830
Lydia <pricelydia776@gmail.com>
2023-03-19 12:13:53 +1300

    Sort Parser and Lexer

--- lexer.py
+++ lexer.py
@@ -2,36 +2,30 @@ from ply import lex
 
 
 class Lexer:
-    def __init__(self):
-        self.lexer = lex.lex(module=self)
-
     tokens = [
         "constant",
         "id",
         "literal",
         "plus",
-        "end",
-        "space",
-        "newline",
-        "tab"
+        "end"
     ]
 
     reserved = {
         'append': 'append',
-        'exit': 'exit',
         'list': 'list',
+        'exit': 'exit',
         'print': 'print',
         'printlength': 'printlength',
         'printwords': 'printwords',
         'printwordcount': 'printwordcount',
-        'reverse': 'reverse',
-        'set': 'set'
+        'set': 'set',
+        'reverse': 'reverse'
     }
 
     tokens = tokens + list(reserved.values())
 
-    t_plus = r'\+'
     t_literal = r'["][a-zA-Z0-9 \W]*["]'  # TODO may not handle /"?
+    t_plus = r'\+'
     t_end = r';'
     t_ignore_space = r'[ ]'
 
@@ -44,11 +38,19 @@ class Lexer:
         t.type = self.reserved.get(t.value, 'id')
         return t
 
+    def __init__(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+
     def t_error(self, t):
         raise IllegalCharacter(f"Illegal character {t.value[0]!r} in statement")
 
-    def get_tokens(self):
-        return self.tokens
+    def test(self, data):
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            print(tok)
 
 
 class IllegalCharacter(Exception):
--- parser.py
+++ parser.py
@@ -1,55 +1,50 @@
 import ply.yacc as yacc
-from lexer import Lexer
+from lexer import *
 
 
 class Parser:
     tokens = Lexer.tokens
 
-    def __init__(self):
-        self.parser = yacc.yacc(module=self, tabmodule=None, debug=True, write_tables=False,)
-        self.lexer = Lexer().lexer
+    def __init__(self, lexer):
+        print("Parser constructor called")
+        self.parser = yacc.yacc(module=self, tabmodule=None, debug=False, write_tables=False)
+        self.lexer = lexer
 
-    def parse(self, input_string):
-        return self.parser.parse(input_string)
+    # DESTRUCTOR
+    def __del__(self):
+        print('Parser destructor called.')
 
-    def p_value(self, p):
-        '''
-        value : id
-              | constant
-              | literal
-        '''
+    def p_statement(self, p):
+        """
+        statement : append_statement
+                  | list_statement
+                  | exit_statement
+                  | print_statement
+                  | printlength_statement
+                  | printwords_statement
+                  | printwordcount_statement
+                  | set_statement
+                  | reverse_statement
+        """
         p[0] = p[1]
-
-    def p_expression(self, p):
-        """
-        expression : value
-                   | value plus value
-        """
-        if len(p) == 2:
-            p[0] = p[1]
-        else:
-            if p[2] == '+':
-                p[0] = p[1] + p[3]
-            else:
-                p[0] = None  # error handling
 
     def p_append_statement(self, p):
         """
         append_statement : append id expression end
         """
-        p[0] = ('append', p[2], p[2], p[3])
+        p[0] = ('append', p[2], p[3])
 
     def p_list_statement(self, p):
         """
         list_statement : list end
         """
-        p[0] = 'list'
+        p[0] = "list"
 
     def p_exit_statement(self, p):
         """
         exit_statement : exit end
         """
-        p[0] = 'exit'
+        p[0] = "exit"
 
     def p_print_statement(self, p):
         """
@@ -87,22 +82,37 @@ class Parser:
         """
         p[0] = ('reverse', p[2])
 
-    def p_statement(self, p):
+    def p_expression(self, p):
         """
-        statement : append_statement
-                  | list_statement
-                  | exit_statement
-                  | print_statement
-                  | printlength_statement
-                  | printwords_statement
-                  | printwordcount_statement
-                  | set_statement
-                  | reverse_statement
+        expression : value
+                   | value plus value
+        """
+        if len(p) == 2:
+            p[0] = p[1]
+        else:
+            if p[2] == '+':
+                p[0] = p[1] + p[3]
+            else:
+                p[0] = None  # error handling
+
+    def p_value(self, p):
+        """
+        value : id
+              | constant
+              | literal
         """
         p[0] = p[1]
 
     def p_error(self, p):
-        if p:
-            print(f"Syntax error at token {p.type}: {p.value}")
-        else:
-            print("Syntax error at EOF")
+        print("Syntax error at", p.value)
+
+    def parse(self, input_string):
+        return self.parser.parse(input_string)
+
+
+# Build the parser
+myLex = Lexer()
+myPars = Parser(myLex)
+text = input("Type input: ")
+result = myPars.parse(text)
+print(result)
